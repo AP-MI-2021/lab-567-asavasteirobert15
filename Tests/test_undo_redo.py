55d840150e38d03d53f3ee2bcc4495614693188c
unknown <asavasteir@gmail.com>
2021-11-17 02:37:30 -0800

    Testele undo_redo mai complexe

--- Tests/test_undo_redo.py
+++ Tests/test_undo_redo.py
@@ -1,36 +1,42 @@
 from Domain.expense import getnewexpense
 from Logic.add_value_if_date import add_value_if_date
-from UI.console import handle_undo, handle_redo
-
-
-def get_data():
-
-    """
-    Functie suport pentru functiile de test
-    """
-
-    return \
-        [
-            getnewexpense(20, 1, 123, '17.08.2018', 'canal'),
-            getnewexpense(30, 2, 200, '12.12.2020', 'intretinere'),
-            getnewexpense(19, 3, 390, '15.05.2019', 'alte cheltuieli'),
-            getnewexpense(8, 2, 332, '05.07.2016', 'canal'),
-            getnewexpense(23, 2, 1000, '12.12.2020', 'canal')
-
-        ]
+from Logic.crud import create
+from UI.console import handle_undo, handle_redo, handle_list_versions
 
 
 def test_undo_redo():
-    list = get_data()
-    list_versions = [[], list]
-    current_version = 1
-    new_list = add_value_if_date(list, '12.12.2020', 300)
-    current_version += 1
-    list_versions = list_versions + new_list
-    list1, current_version = handle_undo(list_versions, current_version)
-    assert list1 == list_versions[current_version]
-    list1, current_version = handle_redo(list_versions, current_version)
-    assert list1 == list_versions[current_version]
+    lista_cheltuiala = []
+    list_version = [lista_cheltuiala]
+    current_version = 0
+    lista_cheltuiala = create(lista_cheltuiala, 1, 20, 200, '12.12.2012', 'canal' )
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala = create(lista_cheltuiala, 2, 19, 130, '12.02.2002', 'canal')
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala = create(lista_cheltuiala, 3, 8, 1000, '12.10.2019', 'intretinere')
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    assert len(lista_cheltuiala) == 2
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    assert len(lista_cheltuiala) == 1
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    assert len(lista_cheltuiala) == 0
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    assert len(lista_cheltuiala) == 0
+    lista_cheltuiala = create(lista_cheltuiala, 1, 20, 200, '12.12.2012', 'canal' )
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala = create(lista_cheltuiala, 2, 19, 130, '12.02.2002', 'canal')
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala = create(lista_cheltuiala, 3, 8, 1000, '12.10.2019', 'intretinere')
+    list_version, current_version = handle_list_versions(list_version, current_version, lista_cheltuiala)
+    lista_cheltuiala, current_version = handle_redo(list_version, current_version)
+    assert len(lista_cheltuiala) == 3
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    lista_cheltuiala, current_version = handle_undo(list_version, current_version)
+    assert len(lista_cheltuiala) == 1
+    lista_cheltuiala, current_version = handle_redo(list_version, current_version)
+    assert len(lista_cheltuiala) == 2
+    lista_cheltuiala, current_version = handle_redo(list_version, current_version)
+    assert len(lista_cheltuiala) == 3
 
 
 
